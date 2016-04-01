259e46b3c25989fac40e0770df7bac12c96ea755
Will Teeple <w933t420@ku.edu>
2016-04-01 15:40:48 -0500

    Add WorldDriver.java to test World class

---
+++ WorldDriver.java
@@ -0,0 +1,19 @@
+/**
+ * @author : Will Teeple
+ * @version : 1
+ * @since 03/30/2016
+ * Description : Driver file to test the World Class
+ */
+
+public class WorldDriver
+{
+  public static void main(String[] args) {
+    World world = new World();
+
+    do
+    {
+      world.displayWorld();
+      world.menuInteraction();
+    } while(world.inWorld());
+  }
+}
