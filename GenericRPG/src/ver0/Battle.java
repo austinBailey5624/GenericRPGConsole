a3c298724665085cb871b6fffb36526225dc97b7
Tim Elvart <telvart@ku.edu>
2016-04-01 14:11:15 -0500

    First commit

--- GenericRPG/src/ver0/Battle.java
+++ GenericRPG/src/ver0/Battle.java
@@ -1,5 +1,20 @@
 package ver0;
 
-public class Battle {
+import java.util.Random;
 
+public class Battle {
+	
+	//member variables
+	boolean m_battleOver;
+
+	//call this when starting a battle event 
+	public void actorBattle(Actor a1, Actor a2)
+	{
+		
+	}
+	
+	public boolean isBattleOver()
+	{
+		return true;
+	}
 }
