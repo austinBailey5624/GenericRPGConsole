3e5afe60b212f0f50ecd127f5b9ab441bfcc8c58
Austin Bailey <a604b997@ku.edu>
2016-04-02 13:39:59 -0500

    Added functionality to Shop class

--- GenericRPG/src/ver0/Shop.java
+++ GenericRPG/src/ver0/Shop.java
@@ -22,7 +22,7 @@ public class Shop
 			m_inventory[7]=10;
 		}
 	}
-	public void displayMenu()
+	public void displayMenu(PlayerActor character)
 	{
 		Item[] itemSet = Item.getAllItems();
 		System.out.println("Welcome to our shop! feel free to buy stuff! We'd like that!");
@@ -39,10 +39,10 @@ public class Shop
 		select=myScanner.nextInt();
 		if(select>0&&select<(choice+1))
 		{
-			displayMenuItemOptions(select);
+			displayMenuItemOptions(select,character);
 		}
 	}
-	public void displayMenuItemOptions(int select)
+	public void displayMenuItemOptions(int select,PlayerActor character)
 	{
 		Item[] itemSet = Item.getAllItems();
 		int choice=1;
@@ -66,16 +66,60 @@ public class Shop
 			select=myScanner.nextInt();
 			if(select==1)
 			{
-				purchase(index);
+				purchase(index,character);
+			}
+			else
+			{
+				return;
+			}
+		}
+		else if(m_inventory[index]>1)
+		{
+			System.out.println("You can buy up to " + m_inventory[index] + "Of these, how many do you want?");
+			int select2=myScanner.nextInt();
+			if(select2>m_inventory[index])
+			{
+				System.out.println("Sorry, we don't have that many " + itemSet[index].getName() + "s");
+			}
+			else if(select2<0)
+			{
+				System.out.println("Sorry, we cant give you a negative number of something, but you can sell it to us!");
+			}
+			else if(select2==0)
+			{
+				System.out.println("Sorry, would you like to buy something else?");
+			}
+			else
+			{
+				purchaseMultiple(index,select2,character);
 			}
 		}
 	}
-	public void purchase(int index)
+	public void purchase(int index,PlayerActor character)
 	{
-		
+		Item[] itemSet = Item.getAllItems();
+		if(character.buyItem(itemSet[index].getValue()))
+		{
+			character.getInventory()[index]++;
+			this.m_inventory[index]--;
+		}
+		else
+		{
+			System.out.println("Sorry, you don't have enough money!");
+		}
 	}
-	public void purchaseMultiple(int index, int quantity)
+	public void purchaseMultiple(int index, int quantity, PlayerActor character)
 	{
-		
+		Item[] itemSet = Item.getAllItems();
+		if(character.buyItems(index, quantity))
+		{
+			character.getInventory()[index]+=quantity;
+			this.m_inventory[index]-=quantity;
+		}
+		else
+		{
+			System.out.println("Sorry, you don't have enough money!");
+			
+		}
 	}
 }
