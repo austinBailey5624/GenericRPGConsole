1cfcca56231630cf58208be5e5678aaa0ef77eeb
Austin Bailey <a604b997@ku.edu>
2016-04-02 11:43:45 -0500

    refine Skill class by taking out unused variables and add functionality for use in battle

--- GenericRPG/src/ver0/Skill.java
+++ GenericRPG/src/ver0/Skill.java
@@ -7,10 +7,6 @@ public class Skill
 	private String m_description;
 	private boolean m_targetsEnemy;
 	private int m_id;
-	private int m_damage;
-	private int m_healthBonus;
-	private double m_attackModifier;
-	private double m_defenseModifier;
 	private double m_accuracy;
 	static int m_numOfSkills=0;
 
@@ -31,41 +27,22 @@ public class Skill
 	}
 
 	//Constructor
-//	public Skill(Actor User,Actor Target)
 	public Skill(int skillNum)
 	{
-		m_damage=0;
-		m_healthBonus=0;
-		m_attackModifier=0;
-		m_defenseModifier=0;
 		m_accuracy=.8;
 		if(skillNum==0)
 		{
 			m_name="Basic Attack";
 			m_description="A basic attack";
 			m_targetsEnemy=true;
-//			if((User.getAttackFighter()-(.5*Target.getDefenseFighter())>0))
-//			{
-//				m_damage=(int) (User.getAttackFighter()-(.5*Target.getDefenseFighter()));
-//			}
-//			else
-//			{
-//				m_damage=1;
-//			}
+
 		}
 		else if(skillNum==1)
 		{
 			m_name="Power Attack";
 			m_description="A very strong attack that is not very accurate";
 			m_targetsEnemy=true;
-//			if((1.5)*User.getAttackFighter()-.5*Target.getDefenseFighter()>0)
-//			{
-//				m_damage=(int)((1.5)*User.getAttackFighter()-.5*Target.getDefenseFighter());
-//			}
-//			else
-//			{
-//				m_damage=1;
-//			}
+
 			m_accuracy=.7;
 		}
 		else if(skillNum==2)
@@ -73,22 +50,13 @@ public class Skill
 			m_name="Threading Needle";
 			m_description="A very accurate attack whose attack power is reduced";
 			m_targetsEnemy=true;
-//			if(.8*User.getAttackFighter()-.5*Target.getDefenseFighter()>0)
-//			{
-//				m_damage=(int)(.8*User.getAttackFighter()-.5*Target.getDefenseFighter());
-//			}
-//			else
-//			{
-//				m_damage=1;
-//			}
+
 		}
 		else if(skillNum==3)
 		{
 			m_name="Shell";
 			m_description="Increase defense or the remainder of the battle";
 			m_targetsEnemy=false;
-			m_attackModifier=1;
-			m_defenseModifier=1.2;
 			m_accuracy=1;
 		}
 		else if(skillNum==4)
@@ -96,42 +64,107 @@ public class Skill
 			m_name="Heal";
 			m_description="Heals the caster for half their attack value";
 			m_targetsEnemy=false;
-//			m_healthBonus=(int) .5*User.getAttackFighter();
 		}
 		else if(skillNum==5)
 		{
 			m_name="Strengthen";
 			m_description="Increases the user's attack power for the duration of the battle";
 			m_targetsEnemy=false;
-			m_attackModifier=1.2;
 		}
 		else if(skillNum==6)
 		{
 			m_name="Penetrating Strike";
 			m_description="Deals a reduced amount of damage but ignores a targets armor";
 			m_targetsEnemy=true;
-	//		m_damage=(int) .5*User.getAttackFighter();
 		}
 		else if(skillNum==7)
 		{
 			m_name="Blood Ritual";
 			m_description="Attack yourself for an increase in attack damage";
 			m_targetsEnemy=false;
-//			m_damage=(int) .5*User.getAttackFighter();
-			m_attackModifier=1.35;
 		}
 		else if(skillNum==8)
 		{
 			m_name="Berserker Rage";
 			m_description="When you are below half health, increase your attack power. Does nothing if above half health";
 			m_targetsEnemy=false;
-//			if((.5)*User.getCurHp()<=User.getMaxHp())
-//			{
-//				m_attackModifier=1.3;
-//			}
+
 		}
 		m_id=m_numOfSkills;
 		m_numOfSkills++;
+	}
+	
+	public void Execute(Actor User, Actor Target)
+	{
+		//TODO incorporate accuracy into calculation using random
+		if(this.m_id==0)
+		{
+			if((User.getAttackFighter()-(.5*Target.getDefenseFighter())>0))
+			{
+				Target.reduceHp((int) (User.getAttackFighter()-(.5*Target.getDefenseFighter())));
+			}
+			else
+			{
+				Target.reduceHp(1);
+			}
+		}
+		else if(this.m_id==1)
+		{
+			if((1.5)*User.getAttackFighter()-.5*Target.getDefenseFighter()>0)
+			{
+				Target.reduceHp((int)((1.5)*User.getAttackFighter()-.5*Target.getDefenseFighter()));
+			}
+			else
+			{
+				Target.reduceHp(1);
+			}
+		}
+		else if(this.m_id==2)
+		{
+			if(.8*User.getAttackFighter()-.5*Target.getDefenseFighter()>0)
+			{
+				Target.reduceHp((int)(.8*User.getAttackFighter()-.5*Target.getDefenseFighter()));
+			}
+			else
+			{
+				Target.reduceHp(1);
+			}
+		}
+		else if(this.m_id==3)
+		{
+			User.setDefenseModifier(1.2);
+		}
+		else if(this.m_id==4)
+		{
+			if(User.getCurHp()+(.5)*User.getAttackFighter()>User.getMaxHp())//The case where healing heals more than possible
+			{
+				User.setCurHp(User.getMaxHp());
+			}
+			else 
+			{
+				User.setCurHp((int)(User.getCurHp()+User.getAttackFighter()*.5));
+			}
+		}
+		else if(this.m_id==5)
+		{
+			User.setAttackModifier(1.2);
+		}
+		else if(this.m_id==6)
+		{
+			Target.reduceHp((int)(.5*User.getAttackFighter()));//calculation doesn't regard the defense of the target
+		}
+		else if(this.m_id==7)
+		{
+			User.reduceHp((int)(User.getAttackFighter()));//attacking self is intentional, its payment for a boost in attack power
+			User.setAttackModifier(1.35);
+		}
+		else if(this.m_id==8)
+		{
+			if(User.getCurHp()<=(.5)*User.getMaxHp())
+			{
+				User.setAttackModifier(1.3);
+			}
+		}
 	}
 	
 	
@@ -168,30 +201,6 @@ public class Skill
 	public void setId(int id)
 	{
 		m_id=id;
-	}
-	public int getDamage()
-	{
-		return m_damage;
-	}
-	public void setDamage(int damage)
-	{
-		m_damage=damage;
-	}
-	public int getHealthBonus()
-	{
-		return m_healthBonus;
-	}
-	public void setHealthBonus(int healthBonus)
-	{
-		m_healthBonus=healthBonus;
-	}
-	public double getAttackModifier()
-	{
-		return m_attackModifier;
-	}
-	public void setAttackModifier(double attackModifier)
-	{
-		m_attackModifier=attackModifier;
 	}
 	public double getAccuracy()
 	{
