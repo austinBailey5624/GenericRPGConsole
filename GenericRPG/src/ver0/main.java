ee5cadffaeafd9b62917cf058a677c6b0cdde754
Austin Bailey <a604b997@ku.edu>
2016-04-03 13:09:07 -0500

    Added developer options to main

--- GenericRPG/src/ver0/main.java
+++ GenericRPG/src/ver0/main.java
@@ -1,24 +1,47 @@
 package ver0;
-
+import java.util.Scanner;
 public class main 
 {
+	static Scanner myScanner = new Scanner(System.in);
+	static int select;
 	public static void main(String[] args)
 	{
+		//gets all skills and prepares them for use in main
 		Skill[] skillSet = Skill.getSkills();
 		
 		//gets all items and prepares them for use in main
 		Item[] itemSet = Item.getAllItems();
 
-		//System.out.println(itemSet[8].getName());
+
 		//gets all enemies and prepares them for use in main
 		EnemyActor[] enemySet = EnemyActor.getEnemies();
-		
-		
-		
-		System.out.println("Number of Items: " + Item.getNumTypesOfItem());
-		System.out.println("Number of Skills: " + Skill.getNumOfSkillsTotal());
-		System.out.println("Number of Enemies: " + EnemyActor.m_numOfEnemies);
-		
+		boolean exit = false;
+		while(!exit)
+		{
+			System.out.println("Welcome To GenericRPG! What would you like to do?\n1) developer options 2)exit");
+			select=myScanner.nextInt();
+			if(select==1)
+			{
+				System.out.println("1) determine number of items, skills and enemies");
+				select=myScanner.nextInt();
+				if(select==1)
+				{
+					System.out.println("Number of Items: " + Item.getNumTypesOfItem());
+					System.out.println("Number of Skills: " + Skill.getNumOfSkillsTotal());
+					System.out.println("Number of Enemies: " + EnemyActor.m_numOfEnemies);
+				}
+			}
+			else if(select==2)
+			{
+				System.out.println("Thank you for playing!");
+				return;
+			}
+			else
+			{
+				System.out.println("Sorry, we didn't undersand your input");
+			}
+		}
+
 		/*
 		 * Battle Testing:
 		 * 
