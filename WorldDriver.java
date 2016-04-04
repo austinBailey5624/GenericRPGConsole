558671bdf6de04fd0c292265c0711f32f3a86d24
Will Teeple <w933t420@ku.edu>
2016-04-04 15:56:46 -0500

    Modify code to handle new town transitions and exiting game

--- WorldDriver.java
+++ WorldDriver.java
@@ -1,6 +1,6 @@
 /**
  * @author : Will Teeple
- * @version : 0.2
+ * @version : 0.3
  * @since 03/30/2016
  * Description : Driver file to test the World Class
  */
@@ -13,42 +13,67 @@ public class WorldDriver
     //file names
     String worldMapFile = "worldLayout.txt";
     String villagevilleFile = "villageville.txt";
+    String awesometownFile = "awesometown.txt";
 
     //create area arrays
     char[][] worldMap = populateArea(worldMapFile);
     char[][] villageville = populateArea(villagevilleFile);
+    char[][] awesometown = populateArea(awesometownFile);
 
     //create area objects
     World world = new World(0, 3, worldMap);
     Town villagevilleTown = new Town(0, 3, villageville);
+    Town awesometownTown = new Town(0, 3, awesometown);
 
     //town array coordinates (x,y)
     int[] vvCoor = {6, 1};
+    int[] atCoor = {6, 5};
 
-    while(true)
+    boolean gameIsActive = true;
+
+    //begin game Welcome
+    System.out.println("Welcome to Generic RPG!\n");
+
+    while(gameIsActive)
     {
+      //begin game with clear terminal screen
+      clearScreen();
+
       //run world
       do
       {
-        world.displayArea();
+        world.displayArea(world.getArea());
         world.menuInteraction();
-      } while(world.inArea());
+        gameIsActive = !world.getGameExit();
+      } while(world.inArea() && gameIsActive);
 
+      //visit VillageVille
       if (world.getCurrentLoc()[0] == vvCoor[0] && world.getCurrentLoc()[1] == vvCoor[1])
       {
         do
         {
-          villagevilleTown.displayArea();
+          villagevilleTown.displayArea(villagevilleTown.getArea());
           villagevilleTown.menuInteraction();
         } while (villagevilleTown.inArea());
       }
+      //visit AwesomeTown
+      else if (world.getCurrentLoc()[0] == atCoor[0] && world.getCurrentLoc()[1] == atCoor[1])
+      do
+      {
+        awesometownTown.displayArea(awesometownTown.getArea());
+        awesometownTown.menuInteraction();
+      } while (awesometownTown.inArea());
 
+      //return to world, return to previous coordinates
       world.setCurrentToPrevious();
     }
+
+    System.out.println("\n\nYou have exited the game. Have a good day!");
   }
 
   public static char[][] populateArea(String fileName)
   {
+    //file i/o
     FileReader in;
     BufferedReader inbr;
     char[][] tempWorld = new char[7][7];
@@ -91,8 +116,12 @@ public class WorldDriver
     return tempWorld;
   }
 
-  public static void transition()
+  public static void clearScreen() //assisted code from StackOverflow, ---->
+  //url: http://stackoverflow.com/questions/4888362/commands-in-java-to-clear-the-screen
   {
-
+    final String ANSI_CLS = "\u001b[2J";
+    final String ANSI_HOME = "\u001b[H";
+    System.out.print(ANSI_CLS + ANSI_HOME);
+    System.out.flush();
   }
 }
