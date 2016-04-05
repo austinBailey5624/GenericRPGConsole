1fe9b5c9fd1fa822ab6baa1dc52f64383b99cdc3
Austin Bailey <a604b997@ku.edu>
2016-04-05 13:45:41 -0500

    Add shop testing functionality to main

--- GenericRPG/src/ver0/main.java
+++ GenericRPG/src/ver0/main.java
@@ -108,29 +108,48 @@ public class main
 		boolean exit = false;
 		while(!exit)
 		{
-			System.out.println("Welcome To GenericRPG! What would you like to do?\n1) developer options 2)exit");
+			System.out.println("Welcome To GenericRPG! What would you like to do?\n1) developer options \n2)exit");
 			select=myScanner.nextInt();
 			if(select==1)
 			{
-				System.out.println("1) determine number of items, skills and enemies\n2) Attempt Equip Item methods");
-				select=myScanner.nextInt();
-				if(select==1)
+				while(!exit)
 				{
-					System.out.println("Number of Items: " + Item.getNumTypesOfItem());
-					System.out.println("Number of Skills: " + Skill.getNumOfSkillsTotal());
-					System.out.println("Number of Enemies: " + EnemyActor.m_numOfEnemies);
-				}
-				else if(select==2)
-				{
-					PlayerActor myPlayerActor = new PlayerActor();
-					int[] testInventory = new int[itemSet.length];
-					for(int i=0; i<itemSet.length; i++)
+					System.out.println("1) determine number of items, skills and enemies\n2) Attempt Equip Item methods\n3) Attempt to use menu\n4) Attempt to use Shop\n5) exit");
+					select=myScanner.nextInt();
+					if(select==1)
 					{
-						testInventory[i]=3;
+						System.out.println("Number of Items: " + Item.getNumTypesOfItem());
+						System.out.println("Number of Skills: " + Skill.getNumOfSkillsTotal());
+						System.out.println("Number of Enemies: " + EnemyActor.m_numOfEnemies);
 					}
-					myPlayerActor.setInventory(testInventory);
-					myPlayerActor.menu();
+					else if(select==2)
+					{
+						PlayerActor myPlayerActor = new PlayerActor();
+						int[] testInventory = new int[itemSet.length];
+						for(int i=0; i<itemSet.length; i++)
+						{
+							testInventory[i]=3;
+						}
+						myPlayerActor.setInventory(testInventory);
+						myPlayerActor.menu();
+					}
+					else if(select==3)
+					{
+						PlayerActor myPlayerActor = new PlayerActor(1);
+						myPlayerActor.menu();
+					}
+					else if(select==4)
+					{
+						Shop thisShop = new Shop(1);
+						PlayerActor p1 = new PlayerActor(1);
+						thisShop.displayBuyMenu(p1);
+					}
+					else if(select==5)
+					{
+						exit=true;
+					}
 				}
+				exit=false;
 			}
 			else if(select==2)
 			{
