feb6b1a1a4ba5437919a5ef3420a71a5ebf03897
Austin Bailey <a604b997@ku.edu>
2016-04-04 21:18:00 -0500

    Commented PlayerActor class

--- GenericRPG/src/ver0/PlayerActor.java
+++ GenericRPG/src/ver0/PlayerActor.java
@@ -11,6 +11,12 @@ public class PlayerActor extends Actor
 	private int[] m_inventory;
 	
 	//Constructor
+    /**
+     * This function creates a playerActor and sets its member variables to their most basic sets
+     * @precondition - 	None
+     * @post - 			Creates an instance of PlayerActor with default values
+     * @return 			An instance of PlayerActor
+     */
 	public PlayerActor()
 	{
 		this.setLevel(1);
@@ -28,27 +34,67 @@ public class PlayerActor extends Actor
 		equipGauntlets(itemSet[12]);
 	}
 	
-	//getters and setters TODO write JavaDocs for methods
+    /**
+     * This function returns the value of m_exp
+     * @precondition - 	PlayerActor Object exists
+     * @post - 			none
+     * @return 			m_ext
+     */
 	public int getExp()
 	{
 		return m_exp;
 	}
+	
+    /**
+     * This function sets the value of m_exp
+     * @precondition - 	PlayerActor Object exists
+     * @post - 			sets m_exp to parameter int exp
+     * @return 	-		void
+     */
 	public void setExp(int exp)
 	{
 		m_exp=exp;
 	}
+	
+    /**
+     * This function returns the value of m_gold
+     * @precondition -	PlayerActor Object exists
+     * @post - 			none
+     * @return 			m_gold
+     */
 	public int getGold()
 	{
 		return m_gold;
 	}
+	
+    /**
+     * This function sets the value of m_gold
+     * @precondition - 	PlayerActor Object exists
+     * @post - 			sets m_gold to parameter int gold
+     * @return 	-		void
+     */
 	public void setGold(int gold)
 	{
 		m_gold=gold;
 	}
+	
+	/**
+	 * This function adds the value in the parameter to the variable m_gold
+	 * @precondition - 	PlayerActor Object exists
+	 * @post - 			sets m_gold to itself plus parameter gold
+	 * @return - 		void
+	 */
 	public void addGold(int gold)
 	{
 		m_gold=m_gold+gold;
 	}
+	
+	/**
+	 * This function attempts to buy an item from the items given index
+	 * @precondition - 	PlayerActor Object existsw with initialized m_inventory
+	 * @post - 			If buy is successful, adds one to the index of the inventory that stands for that item, else returns false
+	 * @return - 		true if buy is successful, false else
+	 */
 	public boolean buyItem(int index)//return true if buy is possible
 	{
 		if(m_gold>=itemSet[index].getValue())
@@ -62,6 +108,13 @@ public class PlayerActor extends Actor
 			return false;
 		}
 	}
+	
+	/**
+	 * This function checks to see if an actor can buy an item
+	 * @precondition - 	PlayerActor Object exists
+	 * @post - 			none //TODO make const on next implementation
+	 * @return - 		true if the item can be bought, false else
+	 */
 	public boolean canBuyItem(int cost)
 	{
 		if(m_gold>=cost)
@@ -73,6 +126,13 @@ public class PlayerActor extends Actor
 			return false;
 		}
 	}
+	
+	/**
+	 * This function attempts to buy a number of items
+	 * @precondition - 	PlayerActor Object existsw with initialized m_inventory
+	 * @post - 			If buy is successful, adds parameter int quantity to the index of the inventory that stands for that item, else returns false
+	 * @return - 		true if buy is successful, false else
+	 */
 	public boolean buyItems(int index, int quantity)
 	{
 		if(m_gold>=itemSet[index].getValue()*quantity)
@@ -86,6 +146,13 @@ public class PlayerActor extends Actor
 			return false;
 		}
 	}
+	
+	/**
+	 * This function checks to see if an actor can buy multiple of an item
+	 * @precondition - 	PlayerActor Object exists
+	 * @post - 			none //TODO make const on next implementation
+	 * @return - 		true if the item can be bought, false else
+	 */
 	public boolean canBuyItems(int cost, int quantity)
 	{
 		if(m_gold>=cost*quantity)
@@ -97,16 +164,36 @@ public class PlayerActor extends Actor
 			return false;
 		}
 	}
+	
+	/**
+	 * This function returns m_inventory
+	 * @precondition- 	PlayerActor Object exists
+	 * @post - 			none //TODO make const
+	 * @return - 		m_inventory
+	 */
 	public int[] getInventory()
 	{
 		return m_inventory;
 	}
+	
+	/**
+	 * This function checks to see if an actor can buy an item
+	 * @precondition - 	PlayerActor Object exists
+	 * @post - 			none //TODO make const on next implementation
+	 * @return - 		true if the item can be bought, false else
+	 */
 	public void setInventory(int[] inventory)
 	{
 		m_inventory=inventory;
 	}
 	
 	//Methods
+	/**
+	 * This function adds exp to a playerCharacter after an enemy is defeated
+	 * @precondiion - 	PlayerActor Object exists
+	 * @post - 			adds to m_exp and checks if the character should level up
+	 * @return - 		void
+	 */
 	public void addExp(int addedExp)
 	{
 		m_exp+=addedExp;
@@ -115,6 +202,13 @@ public class PlayerActor extends Actor
 			levelUp(m_exp-100*this.getLevel());
 		}
 	}
+	
+	/**
+	 * This function is responsible for levelling up the PlayerCharacter
+	 * @precondition - 	PlayerActor Object exists
+	 * @post - 			increments member variables to progress character
+	 * @return - 		void
+	 */
 	public void levelUp(int exp)
 	{
 		m_exp-=100*this.getLevel();
@@ -124,9 +218,16 @@ public class PlayerActor extends Actor
 		setDef(getDef()+1);
 		//TODO implement choice for additional level up reward
 	}
+	
+	/**
+	 * This function handles the menu the user interacts with when the option is selected while the person is in world
+	 * @precondition - 	PlayerActor Object exists
+	 * @post - 			none //TODO make const
+	 * @return - 		void
+	 */
 	public void menu()
 	{
-		System.out.println("1) Access Inventory\n2) Return");
+		System.out.println("1) Access Inventory\n2) Return");//TODO add character screen so user can see current attributes
 		select=myScanner.nextInt();
 		if(select==1)
 		{
@@ -141,6 +242,13 @@ public class PlayerActor extends Actor
 			System.out.println("Sorry, we didn't understand your input");
 		}
 	}
+	
+	/**
+	 * This function handles the user accessing the playerCharacters inventory
+	 * @precondition - 	PlayerActor Object exists
+	 * @post - 			none //TODO make const
+	 * @return - 		void
+	 */
 	public void accessInventory()
 	{
 		Item[] items = Item.getAllItems();
@@ -177,6 +285,13 @@ public class PlayerActor extends Actor
 			}
 		}
 	}
+	
+	/**
+	 * This function displays the inventory
+	 * @precondition - 	PlayerActor Object exists
+	 * @post -			none //TODO make const
+	 * @return - 		void			
+	 */
 	public void displayInventory()
 	{
 		for(int i=0; i<itemSet.length;i++)
@@ -184,6 +299,14 @@ public class PlayerActor extends Actor
 			System.out.println("You have " + getInventory()[i] +" " + itemSet[i].getName() + " In your inventory");
 		}
 	}
+	
+	/**
+	 * This function allows for the equipping of items in the invetory
+	 * @precondition - 	PlayerActor Object exists
+	 * @post - 			equips items if appropriate
+	 * @return - 		void
+	 * @param type
+	 */
 	public void equipMenu(int type)//TODO finish equip method
 	{
 		Item[] items = Item.getAllItems();
