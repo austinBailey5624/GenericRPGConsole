203f35a616133c59fcdf86f39549d5d1b61dc462
Tim Elvart <telvart@ku.edu>
2016-04-06 16:48:56 -0500

    Change main.java

--- GenericRPG/src/ver0/main.java
+++ GenericRPG/src/ver0/main.java
@@ -21,38 +21,6 @@ public class main
 		//System.out.println("Number of Skills: " + Skill.getNumOfSkillsTotal());
 		//System.out.println("Number of Enemies: " + EnemyActor.m_numOfEnemies);
 		
-
-		/*
-		 * Battle Testing
-		 */
-		PlayerActor a1=new PlayerActor(1);
-		EnemyActor a2=new EnemyActor(1);
-		//a1.m_inventory[7]=5;
-		//a1.m_inventory[20]=1;
-		//a1.m_inventory[27]=1;
-		//Actor a1=new Actor();
-		//Actor a2=new Actor();
-
-		Battle battle=new Battle();
-		
-		Item basicsword=new Item(1);
-		a1.equipSword(basicsword);
-		
-		a1.setName("Jeff");
-
-		//a2.setName("Bob");
-		//System.out.println("Jeff:"+a1.getCurHp()+" Bob: "+a2.getCurHp());
-		//System.out.println(a2.getDefenseFighter());
-		//System.out.println(a2.getDefenseFighter());
-		if(battle.actorBattle(a1,a2))
-		{
-			System.out.println("Player won the match");
-		}
-
-		
-
-		
-		
 		System.out.println("Number of Items: " + Item.getNumTypesOfItem());
 		System.out.println("Number of Skills: " + Skill.getNumOfSkillsTotal());
 		System.out.println("Number of Enemies: " + EnemyActor.m_numOfEnemies);
