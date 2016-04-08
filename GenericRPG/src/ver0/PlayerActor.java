4561a6344b2f079694a6e1690dfdc50b0ae89b62
Austin Bailey <a604b997@ku.edu>
2016-04-08 16:16:14 -0500

    fixed levelling up problem

--- GenericRPG/src/ver0/PlayerActor.java
+++ GenericRPG/src/ver0/PlayerActor.java
@@ -245,7 +245,7 @@ public class PlayerActor extends Actor
 	public void addExp(int addedExp)
 	{
 		m_exp+=addedExp;
-		while(m_exp>100*this.getLevel())
+		while(m_exp>=100*this.getLevel())
 		{
 			levelUp(m_exp-100*this.getLevel());
 		}
