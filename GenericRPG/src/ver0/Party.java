41c29fdee8f87f99de9e300098ab3c884a79ca1c
Michael <mikewang.6127@gmail.com>
2016-04-17 14:58:07 -0500

    fix the menu error where accessing the inventory would not handle null items.

--- GenericRPG/src/ver0/Party.java
+++ GenericRPG/src/ver0/Party.java
@@ -148,16 +148,7 @@ public class Party
 			}
 			System.out.println("Which Helmet do you want to equip?");
 		}
-//		else if(type==5)
-//		{
-//			System.out.println("Currently have: " + selected.getEquippedGauntlets().getName() + " Equipped, with Attack Bonus: " + selected.getEquippedGauntlets().getBonusAtk() + " And Defense Bonus: " + selected.getEquippedGauntlets().getBonusDef());
-//			System.out.println("Which Gauntlets do you want to equip?");
-//		}
-//		else if(type==6)
-//		{
-//			System.out.println("Currently have: " + selected.getEquippedBoots().getName() + " Equipped, with Attack Bonus: " + selected.getEquippedBoots().getBonusAtk() + " And Defense Bonus: " + selected.getEquippedBoots().getBonusDef());
-//			System.out.println("Which Boots do you want to equip?");
-//		}
+
 		
 		int[] indexRepresentedByChoice= new int[items.length];
 		for(int i=0;i<items.length;i++)
@@ -264,38 +255,7 @@ public class Party
 					m_inventory[indexRepresentedByChoice[select]]--;
 				}
 			}
-//			else if(type==5)
-//			{
-//				if(m_inventory[indexRepresentedByChoice[select]]<1)
-//				{
-//					System.out.println("You dont have a " + items[indexRepresentedByChoice[select]]+ " to equip!");
-//				}
-//				else
-//				{
-//					if(selected.getEquippedGauntlets().getId()!=12)
-//					{
-//						m_inventory[selected.getEquippedGauntlets().getId()]++;
-//					}
-//					selected.equipGauntlets(items[indexRepresentedByChoice[select]]);
-//					m_inventory[indexRepresentedByChoice[select]]--;
-//				}
-//			}
-//			else if(type==6)
-//			{
-//				if(m_inventory[indexRepresentedByChoice[select]]<1)
-//				{
-//					System.out.println("You dont have a " + items[indexRepresentedByChoice[select]]+ " to equip!");
-//				}
-//				else
-//				{
-//					if(selected.getEquippedBoots().getId()!=13)
-//					{
-//						m_inventory[selected.getEquippedBoots().getId()]++;
-//					}
-//					selected.equipBoots(items[indexRepresentedByChoice[select]]);
-//					m_inventory[indexRepresentedByChoice[select]]--;
-//				}
-//			}
+
 		}
 		else if(select==(choices))
 		{
@@ -355,13 +315,25 @@ public class Party
 			System.out.println("Equipped Helmet: no helmet");
 		}
 	
-//		System.out.println("Equipped Gauntlets: " + selected.getEquippedGauntlets().getName() + " bonus Atack: " + selected.getEquippedGauntlets().getBonusAtk()+ " bonus Defense: " +selected.getEquippedGauntlets().getBonusDef());		
-//		System.out.println("Equipped Boots:     " + selected.getEquippedBoots().getName() + " bonus Attack " + selected.getEquippedBoots().getBonusAtk() + " bonus Defense: " + selected.getEquippedBoots().getBonusDef());
 		System.out.println("Natural Attack:     " + selected.getAtk());
-//		System.out.println("Bonus Attack:       " + (selected.getEquippedSword().getBonusAtk()+selected.getEquippedShield().getBonusAtk()+selected.getEquippedArmor().getBonusAtk()+selected.getEquippedHelmet().getBonusAtk()+selected.getEquippedGauntlets().getBonusAtk()+selected.getEquippedBoots().getBonusAtk()));
+		if((selected.getEquippedSword() != null)&&(selected.getEquippedShield() != null)&&(selected.getEquippedArmor() != null)&&(selected.getEquippedHelmet() != null))
+		{
+			System.out.println("Bonus Attack:       " + (selected.getEquippedSword().getBonusAtk()+selected.getEquippedShield().getBonusAtk()+selected.getEquippedArmor().getBonusAtk()+selected.getEquippedHelmet().getBonusAtk()));
+		}
+		else
+		{
+			System.out.println("Bonus Attack:       0");
+		}
 		System.out.println("Effective Attack:   " + selected.getAttackFighter());
 		System.out.println("Natural Defese:     " + selected.getDef());
-//		System.out.println("Bonus Defense:      " + (selected.getEquippedSword().getBonusDef()+selected.getEquippedShield().getBonusDef()+selected.getEquippedArmor().getBonusDef()+selected.getEquippedHelmet().getBonusDef()+selected.getEquippedGauntlets().getBonusDef()+selected.getEquippedBoots().getBonusDef()));
+		if((selected.getEquippedSword() != null)&&(selected.getEquippedShield() != null)&&(selected.getEquippedArmor() != null)&&(selected.getEquippedHelmet() != null))
+		{
+			System.out.println("Bonus Defense:      " + (selected.getEquippedSword().getBonusDef()+selected.getEquippedShield().getBonusDef()+selected.getEquippedArmor().getBonusDef()+selected.getEquippedHelmet().getBonusDef()));
+		}
+		else
+		{
+			System.out.println("Bonus Defense:      0");
+		}
 		System.out.println("Effective Defense:  " + selected.getDefenseFighter());
 	}
 
@@ -502,7 +474,7 @@ public class Party
 				//System.out.println("You gave invalid input! please try again\n");
 				select=7;
 			}
-			if(select>=0&&select<4)
+			if(select>=0&&select<5)
 			{
 				equipMenu(select,selected);
 			}
