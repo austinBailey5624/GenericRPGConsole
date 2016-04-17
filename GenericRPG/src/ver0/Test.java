fb11827ef546c05d6d274c4b0803e6bca9cd9714
Tim Elvart <telvart@ku.edu>
2016-04-17 12:42:04 -0500

    Make actorBattle return an int. Add Test class

---
+++ GenericRPG/src/ver0/Test.java
@@ -0,0 +1,17 @@
+package ver0;
+
+public class Test 
+{
+	public void runTests(boolean verbose)
+	{
+		if (verbose)
+		{
+			//display the results of every single test performed
+		}
+		else
+		{
+			//show a summary of tests that failed
+		}
+	}
+	//ADD TESTS HERE-- THERE WILL BE ALOT
+}
